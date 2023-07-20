bc4a74f69d9413b27ee257ecda26d3992eb0cb46
Ian kangethe <iankangethe@Ians-MacBook-Pro.local>
2023-07-20 21:22:05 +0300

    did countFactors

---
+++ src/codility/CountFactorsAction.java
@@ -0,0 +1,12 @@
+package codility;
+
+public class CountFactorsAction {
+
+    public static void main(String[] args) {
+
+        CountFactors countFactors = new CountFactors();
+
+        System.out.println("Factors of 24 are: "+ countFactors.solution(24));
+        System.out.println("Factors of 32 are: "+ countFactors.solution(32));
+    }
+}
